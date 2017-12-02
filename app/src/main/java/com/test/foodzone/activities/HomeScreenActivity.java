f9fa3f4e35e40fe99b6d98a07de77f8f59d645ad
gokul kalagara <gokulkalagara@gmail.com>
2017-12-02 16:25:04 +0530

    start

--- app/src/main/java/com/test/foodzone/activities/HomeScreenActivity.java
+++ app/src/main/java/com/test/foodzone/activities/HomeScreenActivity.java
@@ -1,12 +1,15 @@
 package com.test.foodzone.activities;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
@@ -22,6 +25,9 @@ public class HomeScreenActivity extends AppCompatActivity implements IActivity {
 
     @BindView(R.id.navigation)
     BottomNavigationViewEx navigation;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +37,10 @@ public class HomeScreenActivity extends AppCompatActivity implements IActivity {
         navigation.enableShiftingMode(false);
         navigation.enableItemShiftingMode(false);
         navigation.setTextVisibility(false);
+        toolbar.setTitle("Fudnow");
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.parseColor("#909090"));
+
 
 
 
@@ -39,11 +49,17 @@ public class HomeScreenActivity extends AppCompatActivity implements IActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
 
-
-
-
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void changeTitle(String title) {
